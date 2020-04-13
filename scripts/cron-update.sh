48fd54f50bca521756ecca25a39d6c34ff065840
Scott Lindsey <scott@scott.to>
2020-04-12 20:48:46 -0700

    cron'd updates

---
+++ scripts/cron-update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o pipefail
+set -e
+
+__here="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__root="$__here/../"
+
+. $__here/credentials.sh
+
+# update source data
+cd $__root/data/COVID-19
+git pull
+
+#parse data
+cd $__here
+nodejs full-state-data.js
+
+# upload to s3
+aws s3 sync $__root/public/data s3://corona-prod-react/data
